8e37e584cefc67bf3d2037f1d8a71bad838395a1
Overaq <root@vps319254.ovh.net>
2016-10-14 08:59:33 +0200

    update2php

---
+++ EasyGrowing/index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aquilain
+ * Date: 13/10/2016
+ * Time: 12:44
+ */
+include "headerFooter.php";
+echo "
+<!Doctype html>
+<html>
+	<head>
+		<meta charset=\"utf-8\">
+		<title>EasyGrowing</title>
+		<link href=\"index.css\" rel=\"stylesheet\" type=\"text/css\">
+	</head>
+	<body>
+		".$header."
+		<main>
+			<div id=\"titre\" class=\"main\">
+				<h1>PRÉPAREZ-VOUS POUR <br> L'AVENIR</h1>
+			</div>
+			<div class=\"clear\"></div>
+		</main>
+		".$footer."
+	</body>
+</html>
+"
+?>
---
+++ EasyGrowing/shop.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aquilain
+ * Date: 13/10/2016
+ * Time: 12:48
+ */
+include "headerFooter.php";
+echo"
+<!Doctype html>
+<html>
+    <head>
+        <meta charset=\"utf-8\">
+        <title>EasyGrowing</title>
+        <link href=\"index.css\" rel=\"stylesheet\" type=\"text/css\">
+    </head>
+    <body>
+        ".$header."
+    <main>
+        <div id=\"shop\" class=\"main\">
+            <h1>SHOP</h1>
+            <h2>Fleurs</h2>
+            <h2>Fruits et légumes</h2>
+            <h2>Appareillage</h2>
+        </div>
+        <div class=\"clear\"></div>
+	</main>
+        ".$footer."
+    </body>
+</html>
+"
+?>
