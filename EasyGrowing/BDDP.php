d7f749bf3c2dba480e4b5b43c3a7c86476c2fadd
Overaq <aquilain.barvaux@hotmail.com>
2016-10-16 21:28:16 +0200

    ajout img BDDP

--- EasyGrowing/BDDP.php
+++ EasyGrowing/BDDP.php
@@ -1,9 +1,17 @@
 <?php
 session_start();
 include "Constante.php";
-$reqPlantes = $bddP->prepare("SELECT * FROM Plantes ");
+$bddP->exec('SET NAMES utf8');
+$reqPlantes = $bddP->prepare("SELECT * FROM plantes join");
 $reqPlantes->execute();
-$plantesinfo =$reqPlantes->fetch();
+$tableau="";
+while($plantesinfo =$reqPlantes->fetch()){
+    $tableau.="    <tr>
+                    <td><img src=\"img_Plantes/ ".$plantesinfo['addresseImg']."\" alt=\"".$plantesinfo['nomPlantes']."\"></td>
+                    <td>".$plantesinfo['nomPlantes']."</td>
+                    <td>description</td>
+                   </tr>";
+}
 echo "
 <!Doctype html>
 <html>
@@ -14,6 +22,15 @@ echo "
 			<div class=\"main\" id=\"BDDP\">
 				<h1>Base de donnée plantes</h1>
 				".$plantesinfo['nomPlantes']."
+				<Table Borde='1'>
+				    <tr>
+				        <th>Photo</th>
+				        <th>Nom de la plante</th>
+				        <th>Descriptions</th>
+				        <th><a href='#'>Téléchargements</a></th>
+				    </tr>
+				    ".$tableau."
+				</Ttable>
 			</div>
 		</main>
 	</body>
