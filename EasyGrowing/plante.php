878bdd8a5d281c68e114261d6ce554eb15985018
Overaq <aquilain.barvaux@hotmail.com>
2016-10-25 23:15:15 +0200

    ajout_plante_favoris

--- EasyGrowing/plante.php
+++ EasyGrowing/plante.php
@@ -7,6 +7,27 @@ if(isset($_GET['id']) AND $_GET['id']>0) {
     $req = $bdd->prepare('select * From plantes Where id = ?');
     $req->execute(array($getid));
     $info =$req->fetch();
+    if (isset($_SESSION['id'])){
+        $reqmpp=$bdd->prepare("Select * from membre_possede_plante where id_membres = ? AND id_plantes = ? ");
+        $reqmpp->execute(array($_SESSION['id'],$getid));
+        $resultmpp=$reqmpp->rowCount();
+        if ($resultmpp==1){
+            $favorisState="<p>Cette plante est déjà dans vos favoris</p>";
+        }
+        else {
+            if(isset($_POST['formmpp'])){
+                $insertmpp=$bdd->prepare("INSERT into membre_possede_plante (id_membres,id_plantes) values (?,?)");
+                $insertmpp->execute(array($_SESSION['id'],$getid));
+                $favorisState="<p>Cette plante a été ajoutée à vos favoris</p>";
+            }
+            else{
+                $favorisState="<input type=\"submit\" name=\"formmpp\" value=\"Ajouter cette plante\">";
+            }
+        }
+    }
+    else{
+        $favorisState="<p>vous ne pouvez pas ajouter de plante sans compte</p>";
+    }
 }
 else {header('Location:BDDP.php');};
 echo "
@@ -28,6 +49,7 @@ echo "
                                         Humidité du sol : <input type=\"number\" name=\"humdité-terre\" step=\"1\" max=\"100\" min=\"0\" size=\"4\" value=".$info['humiditeSol']."> % <br><br>
                                         Heures d'expositions : <input type=\"time\" name=\"heure_exposition\" value=".$info['heures']."> heure(s) <br><br>
                                         cycle(s) : <input type=\"number\" name=\"cycle\" step=\"1\" max=\"5\" min=\"0\" size=\"4\" value=".$info['cycle']."> cycle(s)
+                                        ".$favorisState."
                                     </form>
                                 </div>
                         <div class=\"clear\"></div>
@@ -37,4 +59,3 @@ echo "
         </html>
     ";
 ?>
-
