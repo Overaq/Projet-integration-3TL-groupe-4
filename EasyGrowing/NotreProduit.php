6d7534c102cf87f31d14d91032d54cc1c0fe9fef
Overaq <aquilain.barvaux@hotmail.com>
2016-11-27 14:49:45 +0100

    ajout notr produit

---
+++ EasyGrowing/NotreProduit.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include "Constante.php";
+?>
+    <html>
+        <?php echo $head ?>
+        <body>
+            <?php echo $header?>
+            <main>
+                <div align="center">
+                    <h2>Notre Produit </h2>
+                    <p>Nous avons mis au point une serre de petite taille entièrement automatisé qui a pour objectif de permettre le dévelopement d'une plante.<br>
+                        De petite ou moyenne taille votre plante pourra se développer dans des conditions optimales.<br>
+                        Le tout ne demande aucune compétance particulière en jardinage ou en automation tous ce que vous avez à faire est de télécharger le profils de la plante que vous souhaité planté.</p>
+                    <p>A l'heure actuel nous disposons seulement d'un protype dont voici les images : </p>
+                    <img  class="imgNP" src="img_Produit/notreProduit%20(1).jpg" alt="image du produit">
+                    <img  class="imgNP" src="img_Produit/notreProduit%20(2).jpg" alt="image du produit">
+                    <img  class="imgNP" src="img_Produit/notreProduit%20(7).jpg" alt="image du produit">
+                    <img  class="imgNP" src="img_Produit/notreProduit%20(8).jpg" alt="image du produit">
+                    <img  class="imgNP" src="img_Produit/notreProduit%20(6).jpg" alt="image du produit">
+                </div>
+            </main>
+        </body>
+    </html>
+
