f92c6fb7e73baaf667e80cdb2c7d3947b1d59e4c
Overaq <aquilain.barvaux@hotmail.com>
2016-11-29 16:28:19 +0100

    Add files via upload

--- EasyGrowing/communicationRasp.php
+++ EasyGrowing/communicationRasp.php
@@ -6,8 +6,9 @@
  * Time: 15:17
  */
 include "Constante.php";
-$getid=$_GET['id'];
-$req = $bdd->prepare('select * From plantes Where id = ?');
+$getid=$_POST['mail'];
+$req = $bdd->prepare('Select mpp.id_m_p as id_m_p, p.addresseImg, p.nomPlantes, p.description, mpp.humidite as humidite, mpp.doses as doses, mpp.temperature as temperature, mpp.humiditeSol as humiditeSol, mpp.heures as heures, mpp.cycle as cycle 
+        from membre_possede_plante as mpp INNER JOIN membres as m on mpp.id_membres=p.id where m.mail = ?');
 $req->execute(array($getid));
 $info =$req->fetch();
 echo $info['humidite'].";".$info['doses'].";".$info['temperature'].";".$info['humiditeSol'].";".$info['heures'].";".$info['cycle'];
