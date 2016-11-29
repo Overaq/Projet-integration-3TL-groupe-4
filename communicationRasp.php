e0a630b2f9df33308f5db2aaabb413bbd7e42e6c
Overaq <aquilain.barvaux@hotmail.com>
2016-11-29 17:26:35 +0100

    Add files via upload

---
+++ communicationRasp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aqui
+ * Date: 29-11-16
+ * Time: 15:17
+ */
+include "Constante.php";
+$getid=$_POST['mail'];
+$req = $bdd->prepare('Select mpp.id_m_p as id_m_p, mpp.humidite as humidite, mpp.doses as doses, mpp.temperature as temperature, mpp.humiditeSol as humiditeSol, mpp.heures as heures, mpp.cycle as cycle
+        from membre_possede_plante as mpp INNER JOIN membres as m on mpp.id_membres=m.id where m.mail = ? ');
+$req->execute(array($getid));
+$info =$req->fetch();
+echo $info['humidite'].";".$info['doses'].";".$info['temperature'].";".$info['humiditeSol'].";".$info['heures'].";".$info['cycle'];
+?>
