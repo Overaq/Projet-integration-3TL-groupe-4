ecc440ac3865fd8ccae9c8ceb03099c1aee73385
Overaq <aquilain.barvaux@hotmail.com>
2016-11-29 17:30:30 +0100

    Add files via upload

--- EasyGrowing/maPlante.php
+++ EasyGrowing/maPlante.php
@@ -5,27 +5,28 @@ $bdd->exec('SET NAMES utf8');
 $bouttonPerso="ceci n'est pas votre plante";
 if(isset($_GET['id_m_p']) AND $_GET['id_m_p']>0 AND $_GET['id'] AND $_GET['id']>0) {
     $getid = intval($_GET['id_m_p']);
+    if(isset($_SESSION['id'])){
+        if($_SESSION['id']==$_GET['id']){
+            if(isset($_POST['formmpp'])){
+                $insertmpp=$bdd->prepare("UPDATE membre_possede_plante SET humidite = ? ,doses = ? ,temperature = ? ,humiditeSol = ? ,heures = ? ,cycle = ? where id_m_p = ?");
+                $insertmpp->execute(array($_POST['humidite'],$_POST['nbrdose'],$_POST['température'],$_POST['humdité-terre'],$_POST['heure_exposition'],$_POST['cycle'],$getid));
+                $bouttonPerso2="<br><p>Modifiée</p><br><input type=\"submit\" name=\"formmpp\" value=\"Enregistre les valeurs\">";
+            }
+            else{
+                $bouttonPerso2="<br><br><input type=\"submit\" name=\"formmpp\" value=\"Enregistre les valeurs\">";
+            }
+        }
+        else{}
+    }
+    else{}
     $req = $bdd->prepare(
         'Select mpp.id_m_p as id_m_p, p.addresseImg, p.nomPlantes, p.description, mpp.humidite as humidite, mpp.doses as doses, mpp.temperature as temperature, mpp.humiditeSol as humiditeSol, mpp.heures as heures, mpp.cycle as cycle 
         from membre_possede_plante as mpp INNER JOIN plantes as p on mpp.id_plantes=p.id where mpp.id_membres = ? and mpp.id_m_p = ?'
     );
     $req->execute(array($_GET['id'],$getid));
     $info =$req->fetch();
-    if(isset($_SESSION['id'])){
-        if($_SESSION['id']==$_GET['id']){
-            $bouttonPerso= "<a href=\"supprimerPlante.php?id=".$info['id_m_p']."\" class=\"supprimerPlante\">supprimer ".$info['nomPlantes']."</a>";
-            if(isset($_POST['formmpp'])){
-                $insertmpp=$bdd->prepare("INSERT into membre_possede_plante (humidite,doses,temperature,humiditeSol,heures,cycle) values (?,?,?,?,?,?,?,?) where id_m_p = ?");
-                $insertmpp->execute(array($_POST['humidite'],$_POST['nbrdose'],$_POST['température'],$_POST['humdité-terre'],$_POST['heure_exposition'],$_POST['cycle'],$getid));
-                $bouttonPerso.="<p>Modifiée</p><br><input type=\"submit\" name=\"formmpp\" value=\"Enregistre les valeurs\">";
-            }
-            else{
-                $bouttonPerso.="<br><br><input type=\"submit\" name=\"formmpp\" value=\"Enregistre les valeurs\">";
-            }
-        }
-        else{}
-    }
-    else{}
+    $bouttonPerso= "<br><br><a href=\"supprimerPlante.php?id=".$info['id_m_p']."\" class=\"supprimerPlante\">supprimer ".$info['nomPlantes']."</a>";
+    $bouttonPerso.$bouttonPerso2;
 }
 else {header('Location:profil.php');};
 echo "
@@ -48,8 +49,9 @@ echo "
                                         <label for=\"humidité-terre\">Humidité du sol : </label> <input id=\"humidité-terre\" type=\"number\" name=\"humdité-terre\" step=\"1\" max=\"100\" min=\"0\" size=\"4\" value=".$info['humiditeSol']."> % <br><br>
                                         <label for=\"heure_exposition\">Heures d'expositions : </label> <input id=\"heure_exposition\" type=\"time\" name=\"heure_exposition\" value=".$info['heures']."> heure(s) <br><br>
                                         <label for=\"cycle\">cycle(s) : </label><input id=\"cycle\" type=\"number\" name=\"cycle\" step=\"1\" max=\"5\" min=\"0\" size=\"4\" value=".$info['cycle']."> cycle(s)
-                                    </form>
                                     ".$bouttonPerso ."
+                                    </form>
+                                    
                                 </div>
                         <div class=\"clear\"></div>
                     </div>
