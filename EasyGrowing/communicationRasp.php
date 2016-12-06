c3e2b235c206490547bf9d7fcb3c033d12b3e3ea
Overaq <aquilain.barvaux@hotmail.com>
2016-12-06 15:09:07 +0100

    Add files via upload

--- EasyGrowing/communicationRasp.php
+++ EasyGrowing/communicationRasp.php
@@ -5,8 +5,14 @@
  * Date: 29-11-16
  * Time: 15:17
  */
+date_default_timezone_set('Europe/Brussels');
 include "Constante.php";
 $getid=htmlspecialchars($_POST['mail']);
+echo $_POST['data_hum'];
+echo $_POST['data_temp'];
+$insertmpp=$bdd->prepare('INSERT INTO data_membre(data_temp,data_hum,data_time,id_membre) SELECT ?,?,?, id from membres WHERE mail = ? ');
+$insertmpp->execute(array(htmlspecialchars($_POST['data_temp']),htmlspecialchars($_POST['data_hum']),date('Y-m-d H:i:s'),htmlspecialchars($_POST['mail'])));
+
 $req = $bdd->prepare('Select mpp.id_m_p as id_m_p, mpp.humidite as humidite, mpp.doses as doses, mpp.temperature as temperature, mpp.humiditeSol as humiditeSol, mpp.heures as heures, mpp.cycle as cycle
         from membre_possede_plante as mpp INNER JOIN membres as m on mpp.id_membres=m.id where m.mail = ? ');
 $req->execute(array($getid));
--- EasyGrowing/profil.php
+++ EasyGrowing/profil.php
@@ -39,7 +39,60 @@ if(isset($_GET['id']) AND $_GET['id']>0) {
             }
             ?>
         </div>
+<p>Flotteur</p>
+<p>Température actuelle: </p>
+<?php
+$requactu= $bdd->prepare('select data_temp ,data_hum, data_time from data_membre where id_membre= ?');
+            $requactu->execute(array($getid)); 
+$actu =$requactu->fetchAll(PDO::FETCH_ASSOC);
+
+//echo print_r($actu);
+
+function inserData($a,$b){
+	$out= "";
+	foreach ($b as $cle => $valeur) {
+	$out.="'".$valeur[$a]."',";}
+	echo substr($out, 0, -1);}
+
+echo end($actu)['data_temp'].'°C';
+?>
+<p>Humidité actuelle: </p>
+<?php echo end($actu)['data_hum'].'%'; ?>
+<p>Dernière mise à jour: </p>
+<?php echo end($actu)['data_time']; ?> (UTC+1) 
+        <canvas id="myChart" width="120" height="50" ></canvas>
     </main>
+   <script src='JS/moment.min.js'></script>
+    <script src='JS/Chart.js'></script>
+    <script>
+    Chart.defaults.global.defaultFontColor="#FFF"
+
+var config = {
+  type: 'line',
+  data: {
+    labels: [<?php inserData('data_time',$actu); ?>],
+    datasets: [{
+      label: "Humidité en %",
+      data: [<?php inserData('data_hum',$actu); ?>],
+	backgroundColor: "rgba(153,255,255,0.4)"
+    }, {
+      label: 'Température en °C',
+      data: [<?php inserData('data_temp',$actu); ?>],
+      backgroundColor: "rgba(255,0,0,0.6)"
+    }]
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        type: 'time'
+      }],
+    },
+  }
+};
+
+var ctx = document.getElementById("myChart").getContext("2d");
+new Chart(ctx, config);
+</script>
     </body>
     </html>
     <?php
