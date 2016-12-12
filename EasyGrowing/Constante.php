750f5bfb9fbf5e294335468eada7182d5c80bb15
Overaq <aquilain.barvaux@hotmail.com>
2016-12-12 18:29:45 +0100

    Update Constante.php

--- EasyGrowing/Constante.php
+++ EasyGrowing/Constante.php
@@ -13,7 +13,7 @@ $nomPage=str_replace("/"," ", $nomPage);
 if ($nomPage['0']==' EasyGrowing profil'){$reset="<meta http-equiv=\"refresh\" content=\"30\">";}
 $head="
     <head>
-        <meta name=\"viewport\" content=\"width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;\" />
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;\" />
                 <meta charset=\"utf-8\">
                 ".$reset."
                 <title>".$nomPage['0']."</title>
